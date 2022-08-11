78fb8d9596120b57afc0c552c6710c14bf8056e0
Norbert Frank Mba <norbertmbafrank@gmail.com>
2022-08-11 08:21:38 +0100

    update to images

---
+++ .history/src/components/section-components/video_20220811081414.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import parse from 'html-react-parser';
+
+class Video extends Component {
+
+    render() {
+
+        let publicUrl = process.env.PUBLIC_URL+'/'
+
+    return <div className="video-area bg-overlay">
+			  <div className="container">
+			    <div className="row justify-content-center">
+			      <div className="col-xl-8 col-lg-10">
+			        <div className="section-title text-center">
+			          <h2>Check it Out</h2>
+			          <p>A show case </p>
+			          <a className="play-btn" href="https://www.youtube.com/embed/Wimkqo8gDZ0" data-effect="mfp-zoom-in"><i className="fa fa-play" /></a>
+			        </div>
+			      </div>
+			    </div>
+			  </div>
+			</div>
+        }
+}
+
+export default Video
