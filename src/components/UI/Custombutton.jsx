afd62b0ed4f56431f4c77a9a26123048080ddc5f
Akpan joseph <syntaxdesignertuts@gmail.com>
2023-01-28 17:44:41 +0100

    finished refactoring signin page

--- src/components/UI/Custombutton.jsx
+++ src/components/UI/Custombutton.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { CustomBtn } from './style'
 
-const Custombutton = ({text,onClick,icon,iconProps,...rest}) => {
+const Custombutton = ({text,onClick,icon,iconProps,className,width,buttonColor,...rest}) => {
 
     const IconComponent = icon
   return (
     <>
-    <CustomBtn onClick={onClick} {...rest}>
+    <CustomBtn style={{background:buttonColor}} className={className} onClick={onClick} {...rest}>
     {!!icon && <IconComponent {...iconProps} />}
     {!!text && text}
-    </CustomBtn>
+    </CustomBtn >
     </>
   )
 }
